567e483bd4fcbf037b9cfe096dc058194a0cc651
Ashu Kumar <ashu8778ak@gmail.com>
2024-05-04 22:57:48 +0530

    controller-create role for user

--- k8s-controller/internal/controller/user_controller.go
+++ k8s-controller/internal/controller/user_controller.go
@@ -18,7 +18,11 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -55,10 +59,46 @@ type UserReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.2/pkg/reconcile
 func (r *UserReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	// TODO: log events
 	_ = log.FromContext(ctx)
+	fmt.Println("---")
 
-	// TODO(user): your logic here
+	// Check if resource exists
+	user := &usermanagementv1.User{}
+	err := r.Get(ctx, req.NamespacedName, user)
+	if err != nil {
+		// If not found, return - no need to create any resource
+		if errors.IsNotFound(err) {
+			fmt.Printf("Resouce user: %v in ns: %v not found\n", req.Name, req.Namespace)
+			return ctrl.Result{}, nil
+		}
+		fmt.Printf("Error retriving user: %v in ns: %v\n", req.Name, req.Namespace)
+		return ctrl.Result{}, err
+	}
 
+	// If found - create role for user
+
+	// Role specifications
+	blockOwnerDeletion := true
+	role := rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      user.Name,
+			Namespace: user.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				{APIVersion: user.APIVersion, Kind: user.Kind, Name: user.Name, BlockOwnerDeletion: &blockOwnerDeletion, UID: user.UID},
+			},
+		},
+		Rules: user.Spec.RoleRules,
+	}
+
+	// Update the role with the user rolr spec
+	err = r.Update(ctx, &role)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	fmt.Printf("Resouce role: %v in ns: %v updated\n", role.Name, role.Namespace)
 	return ctrl.Result{}, nil
 }
 
